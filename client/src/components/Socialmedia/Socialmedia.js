c9f2d13ebd81e612d7c97171775d91c9e93840d9
Curtis Pike <cpike46@gmail.com>
2020-12-05 13:45:18 -0800

    social media changes

--- client/src/components/Socialmedia/Socialmedia.js
+++ client/src/components/Socialmedia/Socialmedia.js
@@ -20,36 +20,26 @@ export default function Socialmedia() {
 		<div className="Socialmedia-Full-Container" id="gallery">
 			<h2 className="Socialmedia-Section-Title">Image Gallery</h2>
 			<MDBContainer>
-				<MDBCarousel activeItem={1} length={6} showControls={true} showIndicators={false} className="z-depth-1" slide>
+				<MDBCarousel activeItem={1} length={4} showControls={true} showIndicators={false} className="z-depth-1" slide>
 					<MDBCarouselInner>
 						<MDBCarouselItem itemId="1">
-								<MDBView>
-									<img className="d-block w-100" src="./Assets/Images/socialmedia5.JPG" alt="First slide" />
+							<MDBView>
+								<img className="d-block w-100" src="./Assets/Images/socialmedia4.JPG" alt="First slide" />
 							</MDBView>
 						</MDBCarouselItem>
 						<MDBCarouselItem itemId="2">
 							<MDBView>
-								<img className="d-block w-100" src="./Assets/Images/socialmedia7.jpg" alt="Second slide" />
+								<img className="d-block w-100" src="./Assets/Images/socialmedia8.JPG" alt="Second slide" />
 							</MDBView>
 						</MDBCarouselItem>
 						<MDBCarouselItem itemId="3">
 							<MDBView>
-								<img className="d-block w-100" src="./Assets/Images/socialmedia4.JPG" alt="Third slide" />
+								<img className="d-block w-100" src="./Assets/Images/socialmedia3.JPG" alt="Third slide" />
 							</MDBView>
 						</MDBCarouselItem>
 						<MDBCarouselItem itemId="4">
 							<MDBView>
-								<img className="d-block w-100" src="./Assets/Images/socialmedia8.JPG" alt="Fourth slide" />
-							</MDBView>
-						</MDBCarouselItem>
-						<MDBCarouselItem itemId="5">
-							<MDBView>
-								<img className="d-block w-100" src="./Assets/Images/socialmedia3.JPG" alt="Fifth slide" />
-							</MDBView>
-						</MDBCarouselItem>
-						<MDBCarouselItem itemId="6">
-							<MDBView>
-								<img className="d-block w-100" src="./Assets/Images/socialmedia1.JPG" alt="Sixth slide" />
+								<img className="d-block w-100" src="./Assets/Images/socialmedia1.JPG" alt="Fourth slide" />
 							</MDBView>
 						</MDBCarouselItem>
 					</MDBCarouselInner>
