36ca26015505e3d301b5adf615232e768996fd8e
Curtis Pike <cpike46@gmail.com>
2021-02-16 18:55:02 -0800

    added new images

--- client/src/components/Socialmedia/Socialmedia.js
+++ client/src/components/Socialmedia/Socialmedia.js
@@ -20,7 +20,7 @@ export default function Socialmedia() {
 		<div className="Socialmedia-Full-Container" id="gallery">
 			<h2 className="Socialmedia-Section-Title">Image Gallery</h2>
 			<MDBContainer>
-				<MDBCarousel activeItem={1} length={8} showControls={true} showIndicators={false} className="z-depth-1" slide>
+				<MDBCarousel activeItem={1} length={13} showControls={true} showIndicators={false} className="z-depth-1" slide>
 					<MDBCarouselInner>
 						<MDBCarouselItem itemId="1">
 							<MDBView>
@@ -60,6 +60,31 @@ export default function Socialmedia() {
 						<MDBCarouselItem itemId="8">
 							<MDBView>
 								<img className="d-block w-100" src="./Assets/Images/socialmedia15.png" alt="Fourth slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="9">
+							<MDBView>
+								<img className="d-block w-100" src="./Assets/Images/socialmedia14.png" alt="Fourth slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="10">
+							<MDBView>
+								<img className="d-block w-100" src="./Assets/Images/socialmedia16.png" alt="Fourth slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="11">
+							<MDBView>
+								<img className="d-block w-100" src="./Assets/Images/socialmedia17.png" alt="Fourth slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="12">
+							<MDBView>
+								<img className="d-block w-100" src="./Assets/Images/socialmedia18.png" alt="Fourth slide" />
+							</MDBView>
+						</MDBCarouselItem>
+						<MDBCarouselItem itemId="13">
+							<MDBView>
+								<img className="d-block w-100" src="./Assets/Images/socialmedia19.png" alt="Fourth slide" />
 							</MDBView>
 						</MDBCarouselItem>
 					</MDBCarouselInner>
